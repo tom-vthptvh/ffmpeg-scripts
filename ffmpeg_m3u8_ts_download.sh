d4cbd783621282cd2c21e6eab6d2c43325c29211
tom-vthptvh <toanvan7@gmail.com>
2018-09-30 21:44:32 +0700

    Update ffmpeg_m3u8_ts_download.sh
    Disable file check 
    Support http url

--- ffmpeg_m3u8_ts_download.sh
+++ ffmpeg_m3u8_ts_download.sh
@@ -3,9 +3,11 @@
 #FILE is url to m3u8 file (local, remote)
 
 FILE=$1
-if [ ! -e "${FILE}" ]; then
-	echo "No input url"
-	exit
-fi
+
+# check if FILE is local file
+#if [ ! -e "${FILE}" ]; then
+#	echo "No input url"
+#	exit
+#fi
 
 ffmpeg -protocol_whitelist "file,http,https,tcp,tls" -i ${FILE} -c copy -bsf:a aac_adtstoasc video.mp4
